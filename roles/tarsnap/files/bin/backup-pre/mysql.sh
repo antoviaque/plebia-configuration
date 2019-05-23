f634316b4e4365c55d76097133a22baced9d8c38
Xavier Antoviaque <xavier@antoviaque.org>
2019-05-23 14:22:08 +0000

    Upgrade to Ubuntu 18.04

--- roles/tarsnap/files/bin/backup-pre/mysql.sh
+++ roles/tarsnap/files/bin/backup-pre/mysql.sh
@@ -4,4 +4,4 @@ TMPDIR=/var/lib/mysql/backup
 
 rm -rf "$TMPDIR"
 mkdir -p "$TMPDIR"
-mysql -e 'show databases' | while read DBNAME; do mysqldump -uroot --complete-insert --events --single-transaction "$DBNAME" > "$TMPDIR/$DBNAME".sql; done
+mysql -e 'show databases' | while read DBNAME; do mysqldump --complete-insert --events --single-transaction "$DBNAME" > "$TMPDIR/$DBNAME".sql; done
